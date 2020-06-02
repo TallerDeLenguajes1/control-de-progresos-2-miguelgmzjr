1ac5e7d5956b6f0a6ebc337faf08f31d5ffa0fbb
miguelgomez <miguel_gomezjr@hotmail.com>
2020-06-02 21:04:24 +0200

    creacion de personaje

--- ControlProgreso/JuegodeRol/Personaje.cs
+++ ControlProgreso/JuegodeRol/Personaje.cs
@@ -7,18 +7,18 @@ namespace JuegodeRol
     class Personaje
     {
         //Datos
-        int velocidad; //1 a 10
-        int destreza; //1 a 5
-        int fuerza; //1 a 10
-        int nivel; //1 a 10
-        int armadura; //1 a 10
+        private int velocidad; //1 a 10
+        private int destreza; //1 a 5
+        private int fuerza; //1 a 10
+        private int nivel; //1 a 10
+        private int armadura; //1 a 10
 
         //Caracteristicas
-        string tipo;
-        string nombre;
-        string apodo;
-        DateTime fechadenacimiento;
-        int salud; //100
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime fechadenacimiento;
+        private int salud; //100
 
         public int Velocidad { get => velocidad; set => velocidad = value; }
         public int Destreza { get => destreza; set => destreza = value; }
